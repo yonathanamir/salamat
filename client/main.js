741211897104e3c2022f93746794c0ae51aad47d
Yonathan <yonathan@prismaphotonics.com>
2021-12-09 14:39:27 +0200

    Geowatch test

--- client/main.js
+++ client/main.js
@@ -1,7 +1,14 @@
-function geo_success(position){
-    str = position.coords.longitude + ', ' + position.coords.latitude
-    alert(str)
-}
+// function geo_success(position){
+//     // str = position.coords.latitude + ', ' + position.coords.longitude
+//     $('.coord .coord-lat')[0].innerHTML = position.coords.latitude
+//     $('.coord .coord-lon')[0].innerHTML = position.coords.longitude
+// }
 
-alert('Test')
-navigator.geolocation.getCurrentPosition(geo_success)
+alert('Test Watch')
+// navigator.geolocation.getCurrentPosition(geo_success)
+
+const watchID = navigator.geolocation.watchPosition((position) => {
+    $('.coord .coord-lat')[0].innerHTML = position.coords.latitude
+    $('.coord .coord-lon')[0].innerHTML = position.coords.longitude
+  });
+  

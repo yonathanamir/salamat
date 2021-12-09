7aff318e776b177320296dcb11c71303aa53309d
Motty Gruda <mottyg1@gmail.com>
2021-12-09 15:38:34 +0200

    first backend

---
+++ server/app.py
@@ -0,0 +1,23 @@
+import sqlite3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/create/player')
+def new_session():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/create/party')
+def new_sn():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
